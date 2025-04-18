c9396b77ff614d91eb546da927147b22ac60cb97
Abdullahkhan80 <abdullahabbdullah8@gmail.com>
2025-04-18 23:12:27 +0500

    initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Componentes/Navbar'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Navbar />
+
+
+    </div>
+  )
+}
+
+export default App
